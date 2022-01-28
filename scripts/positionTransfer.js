5f2b1e15fd5df59b39fdd1fc76d9d55173cb3d68
cpysden-coder <chris_pysden@mac.com>
2022-01-28 11:24:01 -0800

    changed to class transferPosition

--- scripts/positionTransfer.js
+++ scripts/positionTransfer.js
@@ -1,24 +1,26 @@
 // position transfer
 
-function TransferPosition(level) {
-    this.level = level;
-    this.fontSize = 140;
-    this.fontColor = 255;
+class TransferPosition {
+    constructor(level) {
+        this.level = level;
+        this.fontSize = 140;
+        this.fontColor = 255;
+    }
+    update(play) {
+        this.fontSize -= 1;
+        this.fontColor -= 1.5;
+        if (this.fontSize < 1) {
+            //  if the condition is met it will go to the InGamePosition
+            play.goToPosition(new InGamePosition(play.setting, this.level));
+        }
+    }
+    draw(play) {
+        ctx.clearRect(0, 0, play.width, play.height);
+        ctx.font = this.fontSize + "px Comic Sans MS";
+        ctx.textAlign = "center";
+        ctx.fillStyle = "rgba(255, " + this.fontColor + ", " + this.fontColor + ", 1)";
+        ctx.fillText("Get ready for level " + this.level, play.width / 2, play.height / 2);
+    }
 }
 
-TransferPosition.prototype.update = function (play) {
-    this.fontSize -= 1;
-    this.fontColor -= 1.5;
-    if (this.fontSize < 1) {
-        //  if the condition is met it will go to the InGamePosition
-        play.goToPosition(new InGamePosition(play.setting, this.level));
-    }
-};
 
-TransferPosition.prototype.draw = function (play) {
-    ctx.clearRect(0, 0, play.width, play.height);
-    ctx.font = this.fontSize + "px Comic Sans MS";
-    ctx.textAlign = "center";
-    ctx.fillStyle = "rgba(255, " + this.fontColor + ", " + this.fontColor + ", 1)";
-    ctx.fillText("Get ready for level " + this.level, play.width / 2, play.height / 2);
-};
